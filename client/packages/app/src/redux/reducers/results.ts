191812627d3a48caf16c81e0262631290a8308fd
Jozsef M Komjati <jm.komjati@codegarager.net>
2020-06-10 22:17:54 +0100

    Empty previous result

--- client/packages/app/src/redux/reducers/results.ts
+++ client/packages/app/src/redux/reducers/results.ts
@@ -1,9 +1,11 @@
-import { SEARCH_DONE } from "../actions";
+import { SEARCH_DONE, SEARCH_NOT_FOUND } from "../actions";
 import { IAction } from "../../interfaces/IAction";
 import { IPokemon } from "../../interfaces/IPokemon";
 
 export const resultsReducer = (state: IPokemon[] = [], action: IAction) => {
   switch (action.type) {
+    case SEARCH_NOT_FOUND:
+      return []
     case SEARCH_DONE:
       return [action.payload];
   }
